c26041a0fd5980d48d01310ada5c576f1786470c
kkk <kutimskyi@gmail.com>
2023-01-22 15:53:34 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b**2 - 4*a*c);
+  if (d === 0) {
+    let x = -b/(2*a);
+    arr.push(x);
+  } else if (d > 0) {
+    let xOne = (-b + Math.sqrt(d) )/(2*a);
+    let xTwo = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(xOne,xTwo);
+  }
   return arr;
 }
 
